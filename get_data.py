42d25d5845a8fbc370552c7efcfbc9494acd5109
HyunA <foren4012@gmail.com>
2018-05-01 20:41:06 +0900

    commit

--- get_data.py
+++ get_data.py
@@ -1,6 +1,6 @@
 import urllib.parse
 import urllib.request
-import urlopen
+import pandas as pd
 import xml.etree.ElementTree as ET
 
 
@@ -32,6 +32,7 @@ baseDataCrtYm = []
 baseSeq = []
 for dataCrtYm in root.findall("./body/items/item/dataCrtYm") :
     baseDataCrtYm.append(dataCrtYm.text)
+
 for seq in root.findall("./body/items/item/seq") :
     baseSeq.append(seq.text)
 
@@ -42,24 +43,55 @@ for i in range(0,len(baseDataCrtYm)-1) :
 
 # create url of DetailInfoSearch
 DetailInfo = 'http://apis.data.go.kr/B552015/NpsBplcInfoInqireService/getDetailInfoSearch'
-# detailfile = open("C:\\Users\\HyunA\\Desktop\\detailInfo.xml","ab")
+
+# 데이터프레임만들기
+dfcol = ['seq', 'jnngpCnt', 'crrmmNtcAmt']
+df_detailInfo = pd.DataFrame(columns=dfcol)
+i = '1'
 for company_seq in baseSeq :
+
     queryParams = '?serviceKey=' + serviceKey + '&' + urllib.parse.urlencode({urllib.parse.quote_plus('seq'): company_seq })
+
     # request BaseInfoSearch data
     request = urllib.request.Request(DetailInfo + queryParams)
     request.get_method = lambda: 'GET'
-    response_body = urllib.request.urlopen(request).read()
-    root = response_body.getroot()
-    root.findall("./body/item")
-    print(response_body)
-    # write xml file
-    # detailfile.write(response_body)
-# detailfile.close()
 
-#for data in root.iter("row") :
-#    if data.findtext("LINE_NUM").find(LINE_NUM) != -1 :
-#        if data.findtext("SUB_STA_NM").find(subName) != -1 :
-#            string = "승차 : {0}"\n"하차 : {1}".format(data.findtext("RIDE_PASGR_NUM"), data.findtext("ALIGHT_PASGR_NUM"))
-#            return string
+    #xml 파일 파싱하기
+    parseString = ET.parse( urllib.request.urlopen(request))
 
+    #정보제대로 가져오는지 확인
+    root = parseString.getroot()
+    resultcode = root.find('header/resultCode')
+    result_msg = '1st_code ' +resultcode.text
+    num_seq = i+'.'+company_seq
+    print(num_seq)
+    print(result_msg)
 
+    if resultcode.text == '99' :
+        # 데이터 재요청
+        request = urllib.request.Request(DetailInfo + queryParams)
+        request.get_method = lambda: 'GET'
+        parseString = ET.parse(urllib.request.urlopen(request))
+        root = parseString.getroot()
+        resultcode = root.find('header/resultCode')
+        result_msg = '2nd_code ' + resultcode.text
+        jnngpCnt = root.find('body/item/jnngpCnt')
+        crrmmNtcAmt = root.find('body/item/crrmmNtcAmt')
+        print(result_msg)
+    else :
+        jnngpCnt = root.find('body/item/jnngpCnt')
+        crrmmNtcAmt = root.find('body/item/crrmmNtcAmt')
+
+    # 예외처리하기 - 데이터가 없는 경우가 존재
+    try :
+        #데이터프레임에 데이터입력하기
+        df_detailInfo = df_detailInfo.append(pd.Series([company_seq,jnngpCnt.text, crrmmNtcAmt.text],index=dfcol),ignore_index=True)
+    except :
+        error_msg =  company_seq + " sequence number error"
+        print(error_msg)
+
+    i = i+1
+
+print(df_detailInfo)
+
+df_detailInfo.to_csv("C:\\Users\\HyunA\\Documents\\myproject\\openAPI\\detail_info.csv", sep=',', encoding='utf-8')
